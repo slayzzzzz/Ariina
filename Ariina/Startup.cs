58a22d9c6b1cedfae01bb5c8486e79a47d062962
Sijie Zhu <Sijie Zhu>
2017-12-01 16:09:34 -0500

    Add project files.

---
+++ Ariina/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ariina.Startup))]
+namespace Ariina
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
